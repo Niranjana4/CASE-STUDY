ed3c77cb86f3f7a72e6ce916144c087d87c16b7d
Niranjana4 <75379681+Niranjana4@users.noreply.github.com>
2021-02-20 19:32:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 Webcam.set({
-    width: 310,
+    width:310,
     height:300,
-    image_format: 'png',
+    image_format:'png',
     png_quality:90,
     constraints:{facingMode:"environment"}
 });
-
 camera=document.getElementById("camera");
+Webcam.attach('#camera')
 
-Webcam.attach('#camera');
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("image").innerHTML='<img id="ar" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version',ml5.version);
+
+classifier=ml5.imageClassifier('MobileNet',modelloaded);
+
+function modelloaded()
+{
+    console.log('modelloaded');
+}
+
+function check()
+{
+  img=document.getElementById('ar');
+  classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("objectName").innerHTML=results[0].label;
+    }
+}
